6aaa116a76107b2d4a8616cab3ae3b1c854a8ce0
ahmed112233445566778899 <121446307+ahmed112233445566778899@users.noreply.github.com>
2022-12-26 17:26:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1
+{
+    internal class program
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int n1, n2;
+
+                Console.WriteLine("enter your first number :");
+                n1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("enter your second number :");
+                n2 = int.Parse(Console.ReadLine());
+
+                for (int i = n1; i <= n2; i++)
+                {
+                    bool prime = true;
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                            prime = false;
+                    }
+                    if (prime == true)
+                        Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}

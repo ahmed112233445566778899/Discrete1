f963a259513f71ed224b9e7b2b6b9768bdfa0959
ahmed112233445566778899 <121446307+ahmed112233445566778899@users.noreply.github.com>
2022-12-26 17:29:39 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,34 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Project_1
+namespace Project_2
 {
-    internal class program
+    class Program
     {
-        class Program
+        public static decimal i { get; private set; }
+
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
+            int n1, n2;
+            Console.WriteLine("please enter your first number :");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter your last number :");
+            n2 = int.Parse(Console.ReadLine());
+
+            for (int num = n1; num < n2; num++)
             {
-                int n1, n2;
-
-                Console.WriteLine("enter your first number :");
-                n1 = int.Parse(Console.ReadLine());
-
-                Console.WriteLine("enter your second number :");
-                n2 = int.Parse(Console.ReadLine());
-
-                for (int i = n1; i <= n2; i++)
+                decimal sum = 0;
+                for (int i = 1; i < num; i++)
                 {
-                    bool prime = true;
-                    for (int j = 2; j < i; j++)
-                    {
-                        if (i % j == 0)
-                            prime = false;
-                    }
-                    if (prime == true)
-                        Console.WriteLine(i);
+                    if (num % i == 0)
+                        sum = sum + i;
                 }
+                if (sum == num)
+                {
+                    Console.WriteLine(num);
+                }
+
+            }
+        
             }
         }
     }
-}
+
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Project 1")]
+[assembly: AssemblyTitle("Project 2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Project 1")]
+[assembly: AssemblyProduct("Project 2")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1be9bd9d-e2b5-44d3-84d5-09ebed07d884")]
+[assembly: Guid("1c524e05-02b8-4575-8052-38f57621bf07")]
 
 // Version information for an assembly consists of the following four values:
 //
